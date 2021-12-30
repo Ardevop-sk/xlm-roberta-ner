6b985f51b9bd01bd15b744eb222ca6537d5b52ff
Filip Bednárik <bednarik@essential-data.sk>
2021-12-30 17:01:27 +0100

    Add custom labels

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -106,7 +106,10 @@ def add_xlmr_args(parser):
      parser.add_argument('--freeze_model', 
                          action='store_true', default=False,
                          help = "whether to freeze the XLM-R base model and train only the classification heads")
-                                   
+
+#     parser.add_argument("--labels_filter", type=str default=None,
+#                         help = "[Optional] Labels to train")
+
      return parser
 
 
